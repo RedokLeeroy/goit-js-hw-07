193fac093cfbfdf58892577cd604324dc218083d
RedokLeeroy <Lys1999@ukr.net>
2022-06-16 14:14:55 +0300

    .

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+
+const galleryBox = galleryItems.map(({preview , description , original}) => {
+    return `<a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`
+});
+
+gallery.insertAdjacentHTML("afterbegin", galleryBox.join(""));
+
+new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250 });
+
+
+//1) як ми взнаєм про альт.
+//2) питання про on.
+//3) івенти.
